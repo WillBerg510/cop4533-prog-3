23cdfea1b35de0eb695483e6fc2909c8db644a10
Lam Nguyen <149017745+lamukaseedu@users.noreply.github.com>
2026-04-06 17:57:39 -0400

    added function to find solution subsequence, write to output file, added comments

--- src/main.py
+++ src/main.py
@@ -1,30 +1,63 @@
+def find_solution_sequence(m, a, b, i, j):
+    if i == 0 or j == 0:
+        return ""
+
+    # if the characters at this location match, add to sequence and go back 1 character for both strings
+    if a[i-1] == b[j-1]:
+        return find_solution_sequence(m, a, b, i-1, j-1) + a[i-1]
+    # if the characters don't match, go backwards 1 character on each string and check the OPT with the higher value
+    elif m[i-1][j] > m[i][j-1]:
+        return find_solution_sequence(m, a, b, i-1, j)
+    else:
+        return find_solution_sequence(m, a, b, i, j-1)
+
+
 def highest_value_lcs(values, a, b):
+    # initialize empty 2d array
     OPT = [[0] * (len(b) + 1) for i in range(len(a) + 1)]
 
-    for i in range(len(a) + 1):
-        for j in range(len(b) + 1):
-            if i == 0 or j == 0:
-                OPT[i][j] = 0
-            elif a[i - 1] == b[j - 1]:
+    # recursion to find optimal solution through tabulation
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            # if there are matching characters, add value of that character to OPT
+            if a[i - 1] == b[j - 1]:
                 OPT[i][j] = OPT[i - 1][j - 1] + values[a[i - 1]]
+            # otherwise move forward 1 character on each string and choose the OPT with the greater value
             else:
                 OPT[i][j] = max(OPT[i - 1][j], OPT[i][j - 1])
-    return OPT[len(a)][len(b)]
+
+    # find solution
+    seq = find_solution_sequence(OPT, a, b, len(a), len(b))
+
+    return [seq, str(OPT[len(a)][len(b)])]
 
 
 if __name__ == "__main__":
     file_name = input("Input file name without extension: ")
 
+    # read file
     with open(f"../data/{file_name}.in", "r") as file:
         k = int(file.readline())
         alphabet = {}
 
+        # go through k lines and put value of each letter into a dictionary
         for i in range(0, k):
             line = file.readline()
             line = line.split(" ")
             alphabet[line[0]] = int(line[1])
 
+        # read strings A and B and remove any whitespace
         A = file.readline().strip()
         B = file.readline().strip()
 
-        print(highest_value_lcs(alphabet, A, B))
+        # compute solution
+        output = highest_value_lcs(alphabet, A, B)
+
+        # write solution to output file
+        with open(f"../data/{file_name}.out", "w") as file:
+            file.write(f"{output[0]}\n")
+            file.write(output[1])
+
+            print(f"Output file data/{file_name}.out created.")
+
+
